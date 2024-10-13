bbd075791d1ba5586e85adfbd1b521d80e2f287b
DuyGTX <quangduy1997vn@gmail.com>
2024-10-13 22:59:36 +0700

    update checkout

--- WebBanGiay/obj/Debug/net6.0/WebBanGiay.AssemblyInfo.cs
+++ WebBanGiay/obj/Debug/net6.0/WebBanGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6772b61984981bdb067c5cb42b89539c02f555a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
