3f3fbf925349ca2b6b4c15a7dc5adc4a325739c8
NTP-Linh <nguyentranphuonglinh4213@gmail.com>
2024-10-17 14:36:04 +0700

    update

---
+++ WebBanGiay/Models/Size.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanGiay.Models;
+
+public partial class Size
+{
+    public int SizeId { get; set; }
+
+    public string Size1 { get; set; } = null!;
+
+    public virtual ICollection<SanPham> SanPhams { get; } = new List<SanPham>();
+}
