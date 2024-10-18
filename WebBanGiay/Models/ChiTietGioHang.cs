ce0c28c217ee12c1a3a0c379dbf3639b8154027d
DuyGTX <quangduy1997vn@gmail.com>
2024-10-19 02:23:48 +0700

    Update

---
+++ WebBanGiay/Models/ChiTietGioHang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanGiay.Models;
+
+public partial class ChiTietGioHang
+{
+    public int Id { get; set; }
+
+    public int? MaGioHang { get; set; }
+
+    public int? SoLuongSp { get; set; }
+
+    public int? MaSp { get; set; }
+
+    public string? MaNguoi { get; set; }
+
+    public string? Makhuyenmai { get; set; }
+
+    public int? TongTien { get; set; }
+
+    public virtual GioHang? MaGioHangNavigation { get; set; }
+
+    public virtual NguoiDung? MaNguoiNavigation { get; set; }
+
+    public virtual SanPham? MaSpNavigation { get; set; }
+}
