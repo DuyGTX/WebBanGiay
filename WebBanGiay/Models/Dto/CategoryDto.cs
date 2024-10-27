6ee7982004d81bbc0c5167e28ea825071aa9fd09
DuyGTX <quangduy1997vn@gmail.com>
2024-10-27 19:13:06 +0700

    update category (create+edit)

--- WebBanGiay/Models/Dto/CategoryDto.cs
+++ WebBanGiay/Models/Dto/CategoryDto.cs
@@ -6,7 +6,14 @@ namespace WebBanGiay.Models.Dto
 {
 	public class CategoryDto
 	{
+		
+		public int CategoryId { get; set; }
+
 		[Required,MaxLength(100)]
 		public string? CategoryName { get; set; } = " ";
+
+		[Required]
+		public int? BrandId { get; set; }// Thêm thuộc tính BrandName
+
 	}
 }
