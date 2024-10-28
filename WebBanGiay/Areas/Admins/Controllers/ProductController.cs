4b1d9a03e53400e2c62360902e02972d8b43e15a
DuyGTX <quangduy1997vn@gmail.com>
2024-10-28 18:40:54 +0700

    update product (View all prod)

---
+++ WebBanGiay/Areas/Admins/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebBanGiay.Models;
+
+namespace WebBanGiay.Areas.Admins.Controllers
+{
+	[Area("Admins")]
+	public class ProductController : Controller
+	{
+		private readonly DbwebGiayOnlineContext context;
+		private readonly IWebHostEnvironment environment;
+
+		public ProductController(DbwebGiayOnlineContext context, IWebHostEnvironment environment)
+		{
+			this.context = context;
+			this.environment = environment;
+		}
+
+		public IActionResult Index()
+		{
+			var product = context.Shoes
+				.Include(s => s.ShoeItems).ThenInclude(si => si.Size)
+				.Include(s => s.Brand)
+				.Include(s => s.ShoeImages) // Thêm Include ShoeImages
+				.OrderBy(s => s.ShoeId)
+				.ToList();
+			return View(product);
+		}
+	}
+}
