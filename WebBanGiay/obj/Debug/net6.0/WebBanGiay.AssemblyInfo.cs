9bc016797960fa29c689757ed221a30dfb0dc39f
Quang Duy <quangduy1997vn@gmail.com>
2025-03-19 01:49:18 +0700

    login/register to main

--- WebBanGiay/obj/Debug/net6.0/WebBanGiay.AssemblyInfo.cs
+++ WebBanGiay/obj/Debug/net6.0/WebBanGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fefe85a97f519711ad2b2d99fbeaf71272120d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
